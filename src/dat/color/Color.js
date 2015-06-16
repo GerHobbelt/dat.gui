84af5b39f1558e8cf87825063c47b1cb2eb6bf8b
Ger Hobbelt <ger@hobbelt.com>
2015-06-16 16:06:20 +0200

    JSHint --> BUGFIX: removed hard-to-spot axtra `!` not operator which b0rked the entire compare expression for hex format colors.

--- src/dat/color/Color.js
+++ src/dat/color/Color.js
@@ -71,7 +71,7 @@ define([
 
     get: function() {
 
-      if (!this.__state.space !== 'HEX') {
+      if (this.__state.space !== 'HEX') {
         this.__state.hex = math.rgb_to_hex(this.r, this.g, this.b);
       }
 

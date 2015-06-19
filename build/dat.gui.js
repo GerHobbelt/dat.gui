dfa4cfba2faa070ce7411afc50539be9d6449994
Ger Hobbelt <ger@hobbelt.com>
2015-06-19 14:32:19 +0200

    fix builder code: this is another license file: the one in `util/`!

--- build/dat.gui.js
+++ build/dat.gui.js
@@ -1,16 +1,4 @@
-/**
- * dat-gui JavaScript Controller Library
- * http://code.google.com/p/dat-gui
- *
- * Copyright 2011 Data Arts Team, Google Creative Lab
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-
+false
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.

df8d0b683c5dd5fea66f3fc23579b6d68334e1ca
Ger Hobbelt <ger@hobbelt.com>
2015-11-14 00:53:02 +0100

    sys -> util in build tools following deprecation warning in node about sys.inspect.

--- utils/build_color.js
+++ utils/build_color.js
@@ -2,10 +2,10 @@ var minify = (process.argv[2] === 'min');
 
 require('./builder.js').build({
     verbose: true,
-    baseUrl: "../src/",
-    main: "dat/color/Color",
-    out: "../build/dat.color" + (minify ? ".min" : "") + ".js",
+    baseUrl: '../src/',
+    main: 'dat/color/Color',
+    out: '../build/dat.color' + (minify ? '.min' : '') + '.js',
     minify: minify,
-    shortcut: "dat.Color",
+    shortcut: 'dat.Color',
     paths: {}
 });
--- utils/build_gui.js
+++ utils/build_gui.js
@@ -2,11 +2,11 @@ var minify = (process.argv[2] === 'min');
 
 require('./builder.js').build({
     verbose: true,
-    baseUrl: "../src/",
-    main: "dat/gui/GUI",
-    out: "../build/dat.GUI" + (minify ? ".min" : "") + ".js",
+    baseUrl: '../src/',
+    main: 'dat/gui/GUI',
+    out: '../build/dat.GUI' + (minify ? '.min' : '') + '.js',
     minify: minify,
-    shortcut: "dat.GUI",
+    shortcut: 'dat.GUI',
     paths: {},
     umd: {
         ret: 'dat'

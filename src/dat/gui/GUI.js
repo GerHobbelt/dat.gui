8e538be02dbfaf62e6c83c4bed097add7cd95013
Ger Hobbelt <ger@hobbelt.com>
2019-05-17 16:09:30 +0200

    optimize previous merge

--- src/dat/gui/GUI.js
+++ src/dat/gui/GUI.js
@@ -42,7 +42,7 @@ const DEFAULT_DEFAULT_PRESET_NAME = 'Default';
 
 const SUPPORTS_LOCAL_STORAGE = (function() {
   try {
-    return 'localStorage' in window && window.localStorage !== null && window.localStorage !== undefined;
+    return 'localStorage' in window && window.localStorage != null;
   } catch (e) {
     return false;
   }
